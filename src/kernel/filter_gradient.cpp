9b9923c07649c4735690b9e9fa506c1bc04e9a17
0000 <a0000@0000deMacBook-Air.local>
2026-05-17 19:32:57 +0800

    update filter

--- src/kernel/filter_gradient.cpp
+++ src/kernel/filter_gradient.cpp
@@ -125,7 +125,7 @@ void stu_filter_gradient(float& out, const data_struct& data,
                          std::size_t width, std::size_t height) {
     const std::size_t W = width;
     const std::size_t H = height;
-    constexpr float inv9 = 1.0f / 9.0f;
+    constexpr double inv9 = 1.0 / 9.0;
 
     const float* __restrict__ a = data.a.data();
     const float* __restrict__ b = data.b.data();
@@ -140,100 +140,96 @@ void stu_filter_gradient(float& out, const data_struct& data,
     double total = 0.0;
 
     for (std::size_t y = 1; y + 1 < H; ++y) {
-        const std::size_t row_m = (y - 1) * W;
-        const std::size_t row_0 = y * W;
-        const std::size_t row_p = (y + 1) * W;
+        const std::size_t ym1 = (y - 1) * W;
+        const std::size_t y0  = y * W;
+        const std::size_t yp1 = (y + 1) * W;
 
-        const float* __restrict__ am = a + row_m;
-        const float* __restrict__ a0 = a + row_0;
-        const float* __restrict__ ap = a + row_p;
+        auto colsum_a = [&](std::size_t x) -> double {
+            return static_cast<double>(a[ym1 + x]) +
+                   static_cast<double>(a[y0  + x]) +
+                   static_cast<double>(a[yp1 + x]);
+        };
 
-        const float* __restrict__ bm = b + row_m;
-        const float* __restrict__ b0 = b + row_0;
-        const float* __restrict__ bp = b + row_p;
+        auto colsum_b = [&](std::size_t x) -> double {
+            return static_cast<double>(b[ym1 + x]) +
+                   static_cast<double>(b[y0  + x]) +
+                   static_cast<double>(b[yp1 + x]);
+        };
 
-        const float* __restrict__ cm = c + row_m;
-        const float* __restrict__ c0 = c + row_0;
-        const float* __restrict__ cp = c + row_p;
+        auto colsum_c = [&](std::size_t x) -> double {
+            return static_cast<double>(c[ym1 + x]) +
+                   static_cast<double>(c[y0  + x]) +
+                   static_cast<double>(c[yp1 + x]);
+        };
 
-        const float* __restrict__ dm = d + row_m;
-        const float* __restrict__ d0 = d + row_0;
-        const float* __restrict__ dp = d + row_p;
+        double ca0 = colsum_a(0);
+        double ca1 = colsum_a(1);
+        double ca2 = colsum_a(2);
 
-        const float* __restrict__ em = e + row_m;
-        const float* __restrict__ e0 = e + row_0;
-        const float* __restrict__ ep = e + row_p;
+        double cb0 = colsum_b(0);
+        double cb1 = colsum_b(1);
+        double cb2 = colsum_b(2);
 
-        const float* __restrict__ fm = f + row_m;
-        const float* __restrict__ f0 = f + row_0;
-        const float* __restrict__ fp = f + row_p;
-
-        const float* __restrict__ gm = g + row_m;
-        const float* __restrict__ gp = g + row_p;
-
-        const float* __restrict__ hm = h + row_m;
-        const float* __restrict__ hp = h + row_p;
-
-        const float* __restrict__ im = i_ch + row_m;
-        const float* __restrict__ ip = i_ch + row_p;
+        double cc0 = colsum_c(0);
+        double cc1 = colsum_c(1);
+        double cc2 = colsum_c(2);
 
         for (std::size_t x = 1; x + 1 < W; ++x) {
             const std::size_t xm1 = x - 1;
             const std::size_t xp1 = x + 1;
 
-            const double sum_a =
-                static_cast<double>(am[xm1]) + am[x] + am[xp1] +
-                static_cast<double>(a0[xm1]) + a0[x] + a0[xp1] +
-                static_cast<double>(ap[xm1]) + ap[x] + ap[xp1];
-
-            const double sum_b =
-                static_cast<double>(bm[xm1]) + bm[x] + bm[xp1] +
-                static_cast<double>(b0[xm1]) + b0[x] + b0[xp1] +
-                static_cast<double>(bp[xm1]) + bp[x] + bp[xp1];
-
-            const double sum_c =
-                static_cast<double>(cm[xm1]) + cm[x] + cm[xp1] +
-                static_cast<double>(c0[xm1]) + c0[x] + c0[xp1] +
-                static_cast<double>(cp[xm1]) + cp[x] + cp[xp1];
-
-            const float avg_a = static_cast<float>(sum_a * inv9);
-            const float avg_b = static_cast<float>(sum_b * inv9);
-            const float avg_c = static_cast<float>(sum_c * inv9);
+            const float avg_a = static_cast<float>((ca0 + ca1 + ca2) * inv9);
+            const float avg_b = static_cast<float>((cb0 + cb1 + cb2) * inv9);
+            const float avg_c = static_cast<float>((cc0 + cc1 + cc2) * inv9);
 
             const float p1 = avg_a * avg_b + avg_c;
 
             const float sobel_dx =
-                -dm[xm1] + dm[xp1]
-                -2.0f * d0[xm1] + 2.0f * d0[xp1]
-                -dp[xm1] + dp[xp1];
+                -d[ym1 + xm1] + d[ym1 + xp1]
+                -2.0f * d[y0 + xm1] + 2.0f * d[y0 + xp1]
+                -d[yp1 + xm1] + d[yp1 + xp1];
 
             const float sobel_ex =
-                -em[xm1] + em[xp1]
-                -2.0f * e0[xm1] + 2.0f * e0[xp1]
-                -ep[xm1] + ep[xp1];
+                -e[ym1 + xm1] + e[ym1 + xp1]
+                -2.0f * e[y0 + xm1] + 2.0f * e[y0 + xp1]
+                -e[yp1 + xm1] + e[yp1 + xp1];
 
             const float sobel_fx =
-                -fm[xm1] + fm[xp1]
-                -2.0f * f0[xm1] + 2.0f * f0[xp1]
-                -fp[xm1] + fp[xp1];
+                -f[ym1 + xm1] + f[ym1 + xp1]
+                -2.0f * f[y0 + xm1] + 2.0f * f[y0 + xp1]
+                -f[yp1 + xm1] + f[yp1 + xp1];
 
             const float p2 = sobel_dx * sobel_ex + sobel_fx;
 
             const float sobel_gy =
-                -gm[xm1] - 2.0f * gm[x] - gm[xp1]
-                + gp[xm1] + 2.0f * gp[x] + gp[xp1];
+                -g[ym1 + xm1] - 2.0f * g[ym1 + x] - g[ym1 + xp1]
+                + g[yp1 + xm1] + 2.0f * g[yp1 + x] + g[yp1 + xp1];
 
             const float sobel_hy =
-                -hm[xm1] - 2.0f * hm[x] - hm[xp1]
-                + hp[xm1] + 2.0f * hp[x] + hp[xp1];
+                -h[ym1 + xm1] - 2.0f * h[ym1 + x] - h[ym1 + xp1]
+                + h[yp1 + xm1] + 2.0f * h[yp1 + x] + h[yp1 + xp1];
 
             const float sobel_iy =
-                -im[xm1] - 2.0f * im[x] - im[xp1]
-                + ip[xm1] + 2.0f * ip[x] + ip[xp1];
+                -i_ch[ym1 + xm1] - 2.0f * i_ch[ym1 + x] - i_ch[ym1 + xp1]
+                + i_ch[yp1 + xm1] + 2.0f * i_ch[yp1 + x] + i_ch[yp1 + xp1];
 
             const float p3 = sobel_gy * sobel_hy + sobel_iy;
 
             total += static_cast<double>(p1 + p2 + p3);
+
+            if (x + 2 < W) {
+                ca0 = ca1;
+                ca1 = ca2;
+                ca2 = colsum_a(x + 2);
+
+                cb0 = cb1;
+                cb1 = cb2;
+                cb2 = colsum_b(x + 2);
+
+                cc0 = cc1;
+                cc1 = cc2;
+                cc2 = colsum_c(x + 2);
+            }
         }
     }
 
