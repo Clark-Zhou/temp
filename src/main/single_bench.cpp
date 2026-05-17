46a050f1d01a0b633a0121a29fd4d8f5d4bfb061
0000 <a0000@0000deMacBook-Air.local>
2026-05-17 13:45:39 +0800

    bench bitwise

--- src/main/single_bench.cpp
+++ src/main/single_bench.cpp
@@ -7,27 +7,32 @@
 
 #include "bench.h"
 #include "relu.h"
+#include "bitwise.h"
 
 
 int main() {
     std::uint32_t seed = 12345u;
-    constexpr size_t relu_size = 1024000;
-    relu_args relu_args_stu;
-    relu_args relu_args_naive;
-    initialize_relu(&relu_args_stu, relu_size, seed);
-    initialize_relu(&relu_args_naive, relu_size, seed);
-    std::println("\tReLU: vector length={}", relu_size);
+    constexpr size_t bitwise_size = 1024000;
+
+    bitwise_args bitwise_args_stu;
+    bitwise_args bitwise_args_ref;
+
+    initialize_bitwise(&bitwise_args_stu, bitwise_size, seed);
+    initialize_bitwise(&bitwise_args_ref, bitwise_size, seed);
+
+    std::println("\tBitwise: vector length={}", bitwise_size);
 
     std::vector<bench_t> benchmarks = {
-                {"ReLU",
-                 stu_relu_wrapper,
-                 naive_relu_wrapper,
-                 relu_check,
-                 &relu_args_stu,
-                 &relu_args_naive,
-                 BASELINE_RELU,
-                 NAIVE_SPEEDUP_LOWER_BOUND_RELU},
+        {"Bitwise",
+         stu_bitwise_wrapper,
+         naive_bitwise_wrapper,
+         bitwise_check,
+         &bitwise_args_stu,
+         &bitwise_args_ref,
+         BASELINE_BITWISE,
+         NAIVE_SPEEDUP_LOWER_BOUND_BITWISE},
     };
+    
     std::cout << "\nRunning Benchmarks...\n";
     std::cout << "--------------------------------------------------------\n";
     std::cout << std::left << std::setw(25) << "Benchmark" << std::setw(12)
